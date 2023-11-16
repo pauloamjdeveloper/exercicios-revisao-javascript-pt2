0359bbe73414e453c1353db82d56fb244e7a9a39
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-11-16 16:17:44 -0300

    Aplicando solução para o exercício 8

---
+++ src/08-formatar-fonte-do-paragrafo/js/script.js
@@ -0,0 +1,37 @@
+const paragraph = document.getElementById("paragraph");
+
+const colorBlack = "#000000";
+const colorWhite = "#FFFFFF";
+
+let fontColor = colorBlack;
+let backgroundColor = colorWhite;
+let currentSize = window.getComputedStyle(paragraph, null).getPropertyValue("font-size");
+let text = paragraph.innerText;
+
+function changeTextColor() {
+  fontColor = fontColor === colorBlack ? colorWhite : colorBlack;
+  paragraph.style.color = fontColor;
+}
+
+function changeBackgroundColor() {
+  backgroundColor = backgroundColor === colorWhite ? colorBlack : colorWhite;
+  paragraph.style.backgroundColor = backgroundColor;
+}
+
+function increaseFontSize() {
+  currentSize = (parseFloat(currentSize) + 1) + "px";
+  paragraph.style.fontSize = currentSize;
+}
+
+function decreaseFontSize() {
+  currentSize = (parseFloat(currentSize) - 1) + "px";
+  paragraph.style.fontSize = currentSize;
+}
+
+function convertToUppercase() {
+  paragraph.innerText = text.toUpperCase();
+}
+
+function convertToLowercase() {
+  paragraph.innerText = text.toLowerCase();
+}
